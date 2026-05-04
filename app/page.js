4c3074cfd2b0b8e605e7aa1dd84564dd3402f445
Danny <danieltinitali@gmail.com>
2026-05-04 10:55:25 +1000

    fix: disable SSR for App correctly

--- app/page.js
+++ app/page.js
@@ -1,6 +1,13 @@
 'use client'
+
+import dynamic from 'next/dynamic'
+
 export const dynamic = 'force-dynamic'
-import App from '../src/App'
+
+const App = dynamic(() => import('../src/App'), {
+  ssr: false
+})
+
 export default function Page() {
   return <App />
 }
